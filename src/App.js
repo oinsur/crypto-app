76ebcff0ee61903e6a16c9a6de379cce9e2a64db
Omar Insurriaga <oinsur@gmail.com>
2022-06-17 13:38:55 -0600

    search added

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Axios from "axios";
 import Coin from "./components/Coin";
 
 function App() {
-  const [listOfCoins, setListOfCoins] = useState([])
+  const [listOfCoins, setListOfCoins] = useState([]);
+  const [searchWord, setSearchWord] = useState("");
 
   useEffect(() => {
     Axios.get("https://api.coinstats.app/public/v1/coins?skip=0").then(
@@ -13,13 +14,22 @@ function App() {
       }
     );
   }, []);
+
+  const filteredCoins = listOfCoins.filter((coin) => {
+    return coin.name.toLowerCase().includes(searchWord.toLowerCase());
+  });
+
   return (
     <div className="App">
       <div className="cryptoHeader">
-        <input type="text" placeholder="Bitcoin..." />
+        <input type="text" placeholder="Bitcoin..."
+          onChange={(event) => {
+            setSearchWord(event.target.value);
+          }}
+        />
       </div>
       <div className="cryptoDisplay">
-        {listOfCoins.map((coin) => {
+        {filteredCoins.map((coin) => {
           return (
             <Coin 
               name = {coin.name} 
