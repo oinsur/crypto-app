a1af5e52e664b3b9d771587169b61d64f53a7e20
Omar Insurriaga <oinsur@gmail.com>
2022-06-17 11:42:39 -0600

    Axios and API added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect} from 'react';
+import Axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
+      (response) => {
+        console.log(response.data);
+      }
+    );
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="crptoHeader"></div>
+      <div className="cryptoDisplay"></div>
     </div>
   );
 }
