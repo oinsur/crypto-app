aac79aa9d33677a788e00f4e6d7f70a9d1304f6e
Omar Insurriaga <oinsur@gmail.com>
2022-06-17 11:53:12 -0600

    coin names shown

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
 import './App.css';
-import {useEffect} from 'react';
+import {useEffect, useState} from 'react';
 import Axios from 'axios';
 
 function App() {
+  const [listOfCoins, setListOfCoins] = useState([])
+
   useEffect(() => {
     Axios.get("https://api.coinstats.app/public/v1/coins?skip=0&limit=10").then(
       (response) => {
-        console.log(response.data);
+        setListOfCoins(response.data.coins);
       }
     );
   }, []);
   return (
     <div className="App">
       <div className="crptoHeader"></div>
-      <div className="cryptoDisplay"></div>
+      <div className="cryptoDisplay">
+        {listOfCoins.map((coin) => {
+          return <h1> {coin.name}</h1>;
+      })}
+      </div>
     </div>
   );
 }
